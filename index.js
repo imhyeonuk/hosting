2e1676b97118e79276877a047cad294770568bdb
imhyeonuk <gusdnr0316@gmail.com>
2024-01-23 19:31:29 +0900

    money

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
+let money = prompt("받고 싶은 돈을 입력하시오")
 
-
-
-
-let score = prompt("당신의 나이를 입력하세요");
-console.log(parseInt(score))
-console.log(score)
-if(score != null)
- {
-  if (parseInt(score) >= 65) {
-    alert("노년");
-  } 
-  else if (parseInt(score) >= 50) {
-    alert("장년"); }
-    else if (parseInt(score) >= 30) {
-      alert("중년"); }
-      else if (parseInt(score) >= 19) {
-        alert("청년"); }
-        else if (parseInt(score) >= 13) {
-          alert("청소년"); }
-          else if (parseInt(score) >=6) {
-            alert("아동"); }
-
-        
-
-         else if (isNaN(parseInt(score))){
-          alert("nan")
-         }
-
-
-
-
-
-
-
-
-
-  else {
-    alert("영‧유아");
-  }
-}
- 
+ alert("싫어!")
  
