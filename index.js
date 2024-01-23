2dabd191ead1d1b06b98202e1f4547d316fe3bd0
imhyeonuk <gusdnr0316@gmail.com>
2024-01-23 11:13:01 +0900

    연령대

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-function inputName(){
 
-  let name = prompt("이름을 입력하세요")
- document.write(`당신의 이름은 ${name}입니다.`)
- 
- // console.log(`당신의 이름은 ${name}입니다.`)
- 
- 
-   
- }
+
+
+
+let score = prompt("당신의 나이를 입력하세요");
+console.log(parseInt(score))
+console.log(score)
+if(score != null)
+ {
+  if (parseInt(score) >= 65) {
+    alert("노년");
+  } 
+  else if (parseInt(score) >= 50) {
+    alert("장년"); }
+    else if (parseInt(score) >= 30) {
+      alert("중년"); }
+      else if (parseInt(score) >= 19) {
+        alert("청년"); }
+        else if (parseInt(score) >= 13) {
+          alert("청소년"); }
+          else if (parseInt(score) >=6) {
+            alert("아동"); }
+
+        
+
+         else if (isNaN(parseInt(score))){
+          alert("nan")
+         }
+
+
+
+
+
+
+
+
+
+  else {
+    alert("영‧유아");
+  }
+}
  
  
