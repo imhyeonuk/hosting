70af745075667ba23a2b1d708e6e4dbfcba3d192
imhyeonuk <gusdnr0316@gmail.com>
2024-01-25 16:57:11 +0900

    마우스 오버하면 이미지 바꾸기

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const bttn = document.querySelector("button");
-console.log(bttn)
+const imgBox = document.querySelector("#container > img");
 
-bttn.onclick = ()  =>   {
-  document.body.classList.toggle("dark");
-  bttn.innerText==="다크 모드로 바꾸기"? 
-  bttn.innerText = "라이트 모드로 바꾸기" :
-  bttn.innerText = "다크 모드로 바꾸기"
-
-}
-
-
-
-
-const letter = document.querySelector("#contents")
-console.log(letter)
-
-letter.onclick = ()  =>   {
-  letter.classList.toggle("red");
-}
-
+imgBox.addEventListener("mouseover", function() {
+  imgBox.src = "img/KakaoTalk_20240117_233609665.jpg";
+});
+imgBox.addEventListener("mouseout", function() {
+  imgBox.src="img/KakaoTalk_20240111_203933282.jpg";
+});
 
 
 
